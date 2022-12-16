88f3776df6195e90958e0d334ffcb5757d0a1035
Kiffin Gish <kiffin.gish@planet.nl>
2022-12-16 15:54:07 +0100

    capacitor

---
+++ android/app/src/main/java/com/gishtech/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gishtech.myapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.gishtech.myapp',
+    appName: 'angular-material-mobile',
+    webDir: 'dist/angular-material-mobile',
+    bundledWebRuntime: false
+};
+
+export default config;
